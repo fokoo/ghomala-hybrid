8c383c68cdaaa3c2a6b77865e89b5bd25398cc63
fokoo <sergffs@yahoo.com>
2026-02-24 00:57:35 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ghomala-hybrid',
+  webDir: 'www'
+};
+
+export default config;
